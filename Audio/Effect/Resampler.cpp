96d7b018603df606eefe8a0452b9a837460988d4
Zsolt Tóth <horvathjeno1944@msn.com>
2018-05-24 10:01:22 +0200

    Ammended the resampler yet again.

--- Audio/Effect/Resampler.cpp
+++ Audio/Effect/Resampler.cpp
@@ -2,64 +2,82 @@
 #include <cstring>
 namespace Audio {
 
-Resampler::Resampler(int converterType)
-	: converter(nullptr), converterType(converterType)
+float Resampler::getSpeed() const
 {
-	convertData.data_in = inputBuffer;
-	convertData.data_out = outputBuffer;
+	return speed;
+}
+void Resampler::setSpeed(float newSpeed)
+{
+	speed = newSpeed;
+}
+double Resampler::getRatio() const
+{
+	if(input.expired()) return 0;
+	else
+	{
+		return double(outputFramerate) / double(getFramerate()) / double(speed);
+	}
+}
+long Resampler::converterCallback(void *self, float **data)
+{
+	if(self)
+	{
+		pResampler sampler = reinterpret_cast<pResampler>(self);
+		if(sampler->input.expired()) return 0;
+		sPlayable input = sampler->input.lock();
+		long inFrames = long(TINYBUFF/input->getChannelCount());
+		double ratio = sampler->getRatio();
+		if(ratio >= 1.00) // If we are upsampling, we need to put fewer frames in the buffer
+		{
+			inFrames = long(double(inFrames) / ratio);
+		}
+		inFrames = input->pullAudio(sampler->inputBuffer,inFrames,
+									input->getChannelCount(),input->getFramerate());
+		*data = sampler->inputBuffer;
+		return inFrames;
+	} else return 0;
+}
+Resampler::Resampler(int converterType)
+	: converter(nullptr), converterType(converterType), speed(1.0f)
+{
+	;
 }
 void Resampler::cleanBuffers()
 {
 	memset(inputBuffer,0,TINYBUFF * sizeof(float));
-	memset(outputBuffer,0,TINYBUFF * sizeof(float));
 }
 void Resampler::onChangedInput()
 {
 	sPlayable tmp = input.lock();
-	converter = sSamplerate(new Samplerate(converterType,tmp->getChannelCount()));
+	if(converter)
+	{
+		src_delete(converter);
+		int err;
+		converter = src_callback_new(converterCallback,converterType,tmp->getChannelCount(),&err,this);
+	}
 }
 long Resampler::pullAudio(float* output, long maxFrameNum, int channelNum, int frameRate)
 {
 	if(input.expired()) return 0;
-	sPlayable tmp = input.lock();
-	if(channelNum != tmp->getChannelCount()) throw std::runtime_error("Resampler - I/O Channel number mismatch! Please use a panner or channel mixer!");
+	if(channelNum != getChannelCount()) throw std::runtime_error("Resampler - I/O Channel number mismatch! Please use a panner or channel mixer!");
 	cleanBuffers();
-	convertData.src_ratio = double(frameRate) / double(tmp->getFramerate());
-	double greaterDenominator = double(std::max(tmp->getFramerate(),frameRate)) / double(std::min(tmp->getFramerate(),frameRate));
-	int guard = std::min(int(maxFrameNum),TINYBUFF);
-	if(frameRate > tmp->getFramerate()) // Downsample - smaller output
-	{
-		guard = int(double(guard/channelNum) * convertData.src_ratio);
-	} else { // Upsample - smaller input
-		guard = int(double(guard/channelNum) / convertData.src_ratio);
-	}
+	outputFramerate = frameRate;
 	long processedFrames = 0;
 	long readFrames = 0;
 	do {
-		readFrames = tmp->pullAudio(inputBuffer,guard,channelNum,tmp->getFramerate());
-		convertData.input_frames = readFrames;
-		convertData.output_frames = long(double(readFrames)*convertData.src_ratio);
-		converter->process(&convertData);
-		for(long frameCursor = 0; frameCursor < convertData.output_frames; ++frameCursor,++processedFrames)
+		long framesToRead = std::min(long(TINYBUFF/channelNum),maxFrameNum-processedFrames);
+		readFrames = src_callback_read(converter,ratio,framesToRead,outputBuffer);
+		for(long frameCursor = 0; frameCursor < readFrames; ++frameCursor,++processedFrames)
 		{
+			long inputCursor = frameCursor*channelNum;
+			long outputCursor = processedFrames*channelNum;
 			for(int i = 0; i < channelNum; ++i)
 			{
-				output[processedFrames+i] += outputBuffer[frameCursor+i];
+				output[outputCursor+i] += outputBuffer[inputCursor+i];
 			}
 		}
 	} while(readFrames);
 	return processedFrames;
 }
-/*long Resampler::process(float* inBuffer, float* outBuffer, long maxFrames, int channelNum, int frameRate)
-{
-	if(channelNum != channelNumber) throw std::runtime_error("Resampler - I/O Channel number mismatch! Please use a panner or channel mixer!");
-	convertData.src_ratio = double(inputFramerate) / double(frameRate);
-	convertData.input_frames = long(double(maxFrames) * convertData.src_ratio);
-	convertData.output_frames = maxFrames;
-	convertData.data_in = inBuffer;
-	convertData.data_out = outBuffer;
-	converter.process(&convertData);
-	return maxFrames;
-}*/
 
 }
--- Audio/Effect/Resampler.hpp
+++ Audio/Effect/Resampler.hpp
@@ -1,22 +1,29 @@
 #ifndef RESAMPLER_HPP
 #define RESAMPLER_HPP
 #include "../AudioPluginPlayable.hpp"
-#include "../Samplerate.hpp"
+#include <samplerate.h>
 
 namespace Audio {
+DEFINE_CLASS(Resampler)
 class Resampler : public PluginPlayable
 {
 private:
-	sSamplerate converter;
-	SRC_DATA convertData;
 	int converterType;
 	float inputBuffer[TINYBUFF];
 	float outputBuffer[TINYBUFF];
+	SRC_STATE* converter;
+	float speed;
+	int outputFramerate;
 	void cleanBuffers();
+	static long converterCallback(void *self, float **data);
+
+	double getRatio() const;
 public:
 	Resampler(int converterType);
 	virtual void onChangedInput();
 	virtual long pullAudio(float* output, long maxFrameNum, int channelNum, int frameRate);
+	float getSpeed() const;
+	void setSpeed(float newSpeed);
 };
 
 }

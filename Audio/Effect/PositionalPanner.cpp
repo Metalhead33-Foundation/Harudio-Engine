2a413a45dd1b6245077b151489ea57fcecd287e6
Zsolt Tóth <horvathjeno1944@msn.com>
2018-06-19 18:23:41 +0200

    Now we have 3D positional audio! Yay!

---
+++ Audio/Effect/PositionalPanner.cpp
@@ -0,0 +1,6 @@
+#include "PositionalPanner.hpp"
+
+PositionalPanner::PositionalPanner()
+{
+
+}
---
+++ Audio/Effect/PositionalPanner.hpp
@@ -0,0 +1,11 @@
+#ifndef POSITIONALPANNER_HPP
+#define POSITIONALPANNER_HPP
+
+
+class PositionalPanner
+{
+public:
+	PositionalPanner();
+};
+
+#endif // POSITIONALPANNER_HPP
